4cbc79ce7f6b5f5eee1a08b61cf081c1c92ff126
Chidera1208 <adaoftheking@gmail.com>
2022-12-10 09:49:32 +0100

    my first OOP

--- OOP.js
+++ OOP.js
@@ -13,3 +13,19 @@ const germanShepherd = new Dogs()
 germanShepherd.hairColor = "brown and black"
 Dogs.numberOfEars = 2
 console.log(germanShepherd, Dogs)
+
+// class of dogs
+class Dogs {
+    numberOfEyes = 2
+    numberOfEars = 2
+    partOfBody = "whiskers"
+    hairColor = "brown"
+    
+    static numberOfLegs = 4 
+    static partOfBody = "tail"
+}
+// object
+const germanShepherd = new Dogs()
+germanShepherd.hairColor = "brown and black"
+Dogs.numberOfEars = 2
+console.log(germanShepherd, Dogs)
